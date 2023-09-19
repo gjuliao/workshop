2bed72fda229fdd01e80b8da0c05a8fa8ed75487
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-19 12:57:39 -0500

    Addes data to seed file and event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     validates :name, :description, presence: true
     validates :start_date, :end_date, :start_time, :end_time, presence: true
-    validates :total_sits, :remaining_sits, :registration_fee, presence: true
+    validates :total_sits, :remaining_sits, :registration_fee, presence: true, numericality: true
+    validates :end_date, comparison: { greater_than: :start_date, message: 'Can not be before starting date' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating events'
+
+events = Event.create([
+    {
+        name: 'Full Stack Event',
+        description: 'Invitation for all ruby on rails passionate',
+        start_date: Date.today + 2.days,
+        end_date: Date.today + 9.days,
+        start_time: '10am',
+        end_time: '5pm',
+        total_sits: 100,
+        remaining_sits: 0,
+        registration_fee: 100
+    },
+    {
+        name: 'React Event',
+        description: 'Invitation for all react passionate',
+        start_date: Date.today + 2.days,
+        end_date: Date.today + 9.days,
+        start_time: '10am',
+        end_time: '5pm',
+        total_sits: 200,
+        remaining_sits: 10,
+        registration_fee: 200
+    },
+    {
+        name: 'Python Event',
+        description: 'Invitation for all python passionate',
+        start_date: Date.today + 2.days,
+        end_date: Date.today + 9.days,
+        start_time: '10am',
+        end_time: '5pm',
+        total_sits: 300,
+        remaining_sits: 25,
+        registration_fee: 300
+    }
+])
+
+
+puts 'Events created'
