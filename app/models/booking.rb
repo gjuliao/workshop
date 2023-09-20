4a5ae8fb81a1613bc818adf29384b7b011eeb05a
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-20 17:18:44 -0500

    Added the method to update event_sit_count

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :customer
   belongs_to :event
+
+  after_create :update_event_sit_count
+
+  def update_event_sit_count
+    event.update(remaining_sits: event.total_sits - no_of_tickets)
+  end
 end
