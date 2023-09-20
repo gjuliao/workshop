0c7cb4b4b3c726091c99c5d01c6d118949605983
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-20 17:12:39 -0500

    Added the has_many validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+    has_many :bookings
+    has_many :events, through: :bookings
+    
     validates :full_name, :contact_number, presence: true
     validates :email, presence: true, uniqueness: true
 end
