5ce0ff4146f606222250c57624f80b3406ae7c03
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-20 16:28:09 -0500

    Added events resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resources :events, only: %i[index show]
 end
