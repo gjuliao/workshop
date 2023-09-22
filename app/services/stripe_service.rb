96c316b8852ef078ce95489d40b10549d24cd52f
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-22 14:35:59 -0500

    Added the stripe charge method

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -26,4 +26,14 @@ class StripeService
             { source: 'tok_mastercard' }
         )
     end
+
+    def create_stripe_charge(amount_to_be_paid, stripe_customer_id, card_id, event)
+        Stripe::Charge.create({
+            amount: amount_to_be_paid * 100,
+            currency: 'usd',
+            source: card_id,
+            customer: stripe_customer_id,
+            description: "Amount $#{amount_to_be_paid} charged for #{event.name}",
+          })
+    end
 end
