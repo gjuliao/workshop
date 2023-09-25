dddf5cb378fa8161ceaf26f31ac956ac0783ff92
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 12:23:15 -0500

    Added the alert messages to app layout

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ApplicationRecord
     has_many :bookings
     has_many :events, through: :bookings
-    
     validates :full_name, :contact_number, presence: true
     validates :email, presence: true, uniqueness: true
 end
