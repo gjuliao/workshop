b4e987a29b34d867f61503e14a3a8c29322fdfa6
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 10:29:45 -0500

    Added config to booking mailer

--- app/mailers/bookings_mailer.rb
+++ app/mailers/bookings_mailer.rb
@@ -1,13 +1,10 @@
 class BookingsMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.bookings_mailer.booking_confirmation.subject
-  #
-  def booking_confirmation
-    @greeting = "Hi"
+  def booking_confirmation(booking)
+    @booking = booking
+    @customer = booking.customer
+    @event = booking.event
+    mail to: @customer.email, subject: 'Blling confirmation for #{@event.name}'
 
-    mail to: "to@example.org"
   end
 end
