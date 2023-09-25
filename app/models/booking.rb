37974f101dbe64eec136c9e9dda7546d8a3c21c9
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 18:55:22 -0500

    Added before validation method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,9 +2,17 @@ class Booking < ApplicationRecord
   belongs_to :customer
   belongs_to :event
 
+  validates :order_number, presence: true, uniqueness: true
+
+  before_validation :generate_order_number
+
   after_create :update_event_sit_count
 
   def update_event_sit_count
     event.update(remaining_sits: event.total_sits - no_of_tickets)
+  end
+
+  def generate_order_number
+    self.order_number = "BOOKING-#{SecureRandom.hex(5).upcase}"
   end
 end
