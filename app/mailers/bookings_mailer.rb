b788117ae5a7e0833233267ea385bc6075391356
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 10:36:05 -0500

    Fixed bug in bookings mailer

--- app/mailers/bookings_mailer.rb
+++ app/mailers/bookings_mailer.rb
@@ -4,7 +4,7 @@ class BookingsMailer < ApplicationMailer
     @booking = booking
     @customer = booking.customer
     @event = booking.event
-    mail to: @customer.email, subject: 'Blling confirmation for #{@event.name}'
+    mail to: @customer.email, subject: "Blling confirmation for #{@event.name}"
 
   end
 end
