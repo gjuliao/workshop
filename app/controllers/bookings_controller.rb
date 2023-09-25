3497fecdfac6807321721bc6b8e5492dfd7e1e9c
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 08:36:50 -0500

    Added params to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,14 @@
 class BookingsController < ApplicationController
     def create
     end 
+
+    private 
+
+    def customer_params
+        params.permit(:full_name, :contact_number, :email)
+    end
+
+    def card_token_params
+        params.permit(:card_number, :exp_month, :exp_year, :cvv)
+    end
 end
