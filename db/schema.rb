e76b7812040e849b4f5d1a21dd5a4646c118459a
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-25 17:31:18 -0500

    Installed friendly gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_221513) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_221632) do
   create_table "bookings", force: :cascade do |t|
     t.integer "no_of_tickets"
     t.float "amount_paid"
@@ -46,6 +46,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_221513) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.index ["slug"], name: "index_events_on_slug", unique: true
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   add_foreign_key "bookings", "customers"
