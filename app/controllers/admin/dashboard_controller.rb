5327f6d92e7fd4823e6a57d2254c3f3e8e454cc0
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-26 14:35:49 -0500

    Added dashboard controller and set variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < AdminController
     def index
+        @event_count = Event.count 
+        @upcoming_events_count = Event.upcoming_events.count
+        @past_events_count = Event.past_events.count
+        @customer_count = Customer.count
+        @booking_count = Booking.count
+        @revenue_earned = Booking.pluck(:amount_paid).sum
     end
 end
