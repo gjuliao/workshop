11df13d9f6ae0e610998ad9904436f35168677dd
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-26 15:18:46 -0500

    Added create method in refunds controller

---
+++ app/controllers/refunds_controller.rb
@@ -0,0 +1,23 @@
+class RefundsController < ApplicationController
+    def new
+        @refund = Refund.new
+    end
+
+    def edit
+        @refund = Refund.find(params[:id])
+    end
+
+    def create
+       booking = Booking.find_by(order_number: params[:refund][:order_number])
+       if booking.present?
+        @refund = Refund.create(
+            customer_id: booking.customer_id,
+            booking_id: booking.id,
+            state: 'accepted'
+        )
+        redirect_to edit_refund_path(@refund), notice: 'Your booking request is valid.  Please fill other details to process request.'
+       else
+        redirect_to new_refund_path, alert: "You provided an invalid booking ID #{params[:refund][:order_number]}. No booking with this number."
+       end
+    end
+end
