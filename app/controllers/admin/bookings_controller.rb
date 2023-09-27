79bfcfb09259e8515128a8b905953fd17739b978
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-27 14:58:51 -0500

    Added eagor loading to admin bookings

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,6 +1,13 @@
 class Admin::BookingsController < AdminController
     before_action :set_booking_with_parents, only: %i[show]
 
+    def index
+        @bookings = Booking.includes(:customer, :event)
+    end
+
+    def show
+    end
+
     private
 
     def set_booking_with_parents
