81683924789e13c59391e6d44b5fb36b0e8ce2ea
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-27 10:16:39 -0500

    Added admin refund notification to mailer

--- app/mailers/refund_notification_mailer.rb
+++ app/mailers/refund_notification_mailer.rb
@@ -11,6 +11,12 @@ class RefundNotificationMailer < ApplicationMailer
   end
 
  
-  def admin_refund_notification
+  def admin_refund_notification(refund)
+    @refund = refund
+    @booking = refund.booking
+    @customer = refund.customer
+    @event = @booking.event
+    @refundable_amount = @refund.no_of_tickets * @event.registration_fee
+    mail to: AdminUser.first.email, subject: "Refund request coming for #{@event.name}."
   end
 end
