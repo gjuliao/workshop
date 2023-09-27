db5bb4ef52a4ba879618a3acc6f65ae413623365
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-27 15:42:15 -0500

    Added ransack gem

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -2,7 +2,8 @@ class Admin::BookingsController < AdminController
     before_action :set_booking_with_parents, only: %i[show]
 
     def index
-        @bookings = Booking.includes(:customer, :event)
+        @q = Booking.ransack(params[:q])
+        @bookings = @q.result.includes(:customer, :event)
     end
 
     def show
