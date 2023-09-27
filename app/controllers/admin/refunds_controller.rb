5ab952238a1abb9e427d7d2862a29ae2a4cee007
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-27 14:37:03 -0500

    Fixed bug in refunds process_refund method

--- app/controllers/admin/refunds_controller.rb
+++ app/controllers/admin/refunds_controller.rb
@@ -2,6 +2,21 @@ class Admin::RefundsController < AdminController
     before_action :set_refund
 
     def process_refund
+        stripe_service = StripeService.new
+        @refund.amount_refunded = @refund.amount_to_be_refunded
+        stripe_charge_id = @refund.booking.stripe_transaction_id
+        stripe_refund = stripe_service.create_stripe_refund(stripe_charge_id)
+        @refund.stripe_refund_id = stripe_refund.id
+        if @refund.amount_refunded == @refund.booking.amount_paid
+            @refund.is_partial_refund = false
+        else
+            @refund.is_partial_refund = true
+        end
+        @refund.state = 'success'
+        @refund.save
+        redirect_to admin_dashboard_path, notice: 'Refund processed succesfully'
+    rescue Stripe::StripeError => error
+        redirect_to admin_dashboard_path, alert: error.message
     end
 
     private
