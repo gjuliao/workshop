b9f436bbffcfdc74f9e5647e2acba2de1dd3e904
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-27 08:08:52 -0500

    Fixed bug in refunds create method

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -10,12 +10,16 @@ class RefundsController < ApplicationController
     def create
        booking = Booking.find_by(order_number: params[:refund][:order_number])
        if booking.present?
-        @refund = Refund.create(
-            customer_id: booking.customer_id,
-            booking_id: booking.id,
-            state: 'accepted'
-        )
-        redirect_to edit_refund_path(@refund), notice: 'Your booking request is valid.  Please fill other details to process request.'
+        if booking.event.start_date > Date.today
+            @refund = Refund.create(
+                customer_id: booking.customer_id,
+                booking_id: booking.id,
+                state: 'accepted'
+            )
+            redirect_to edit_refund_path(@refund), notice: 'Your booking request is valid.  Please fill other details to process request.'
+        else
+            redirect_to new_refund_path, alert:'You are requesting for past workshop. Refunds are only allowed for future events.'
+        end
        else
         redirect_to new_refund_path, alert: "You provided an invalid booking ID #{params[:refund][:order_number]}. No booking with this number."
        end
