f5110b8901281e6cf1cb4b59c5e0ee5f19bd041c
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-29 10:18:53 -0500

    Added daily event hours validation

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -48,5 +48,21 @@ RSpec.describe Event, type: :model do
         event.save
         expect(event.total_duration).to eq("From #{event.start_date} to #{event.end_date}")
     end
+
+    it 'validates daily event hours' do
+        event = Event.new(
+            name: 'Javascript',
+            description: 'This is a javascript event',
+            start_date: Date.today - 2.days,
+            end_date: Date.today + 2.days,
+            start_time: '10am',
+            end_time: '5pm',
+            total_sits: 100,
+            remaining_sits: 100,
+            registration_fee: 100
+        )
+        event.save
+        expect(event.daily_event_hours).to eq("#{((event.start_time.to_time - event.end_time.to_time)/1.hours).abs} hours")
+    end
     
 end
