635d0e6f3875b8ff950298551f66b82335886cea
Giovanni Juliao <gjuliao32@gmail.com>
2023-09-29 10:16:50 -0500

    Added total duration rspec test to event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,5 +16,37 @@ RSpec.describe Event, type: :model do
         event.save
         expect(event.valid?).to be(false)
     end
+
+    it 'validates start date is before end date' do
+        event = Event.new(
+            name: 'Javascript',
+            description: 'This is a javascript event',
+            start_date: Date.today + 2.days,
+            end_date: Date.today - 2.days,
+            start_time: '10am',
+            end_time: '5pm',
+            total_sits: 100,
+            remaining_sits: 100,
+            registration_fee: 100
+        )
+        event.save
+        expect(event.valid?).to be(false)
+    end
+
+    it 'validates total duration' do
+        event = Event.new(
+            name: 'Javascript',
+            description: 'This is a javascript event',
+            start_date: Date.today - 2.days,
+            end_date: Date.today + 2.days,
+            start_time: '10am',
+            end_time: '5pm',
+            total_sits: 100,
+            remaining_sits: 100,
+            registration_fee: 100
+        )
+        event.save
+        expect(event.total_duration).to eq("From #{event.start_date} to #{event.end_date}")
+    end
     
 end
